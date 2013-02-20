42741fce6228b8ea89752ea665a93b3adb0d88f2
Michael Nisi <michael.nisi@gmail.com>
2013-02-20 19:14:37 +0100

    Merge examples

--- example/starred.js
+++ example/starred.js
@@ -1,21 +1,43 @@
 
-// starred - download starred Google Reader images
+// starred - download all images from starred Google Reader items
 
 var boros = require('../index.js')
   , request = require('request')
-  , imgscraper = require('imgscraper')
+  , imgscraper = require('scrimg')
+  , trumpet = require('trumpet')
   , request = require('request')
   , fs = require('fs')
-  , images = imgscraper()
+  , img = imgscraper()
+  , next = null
+  
+var max = 1
+  , pageCount = 0
   , dir = '/tmp/boros-' + Math.floor(Math.random() * (1<<24))
-  , uri = 'https://www.google.com/reader/shared/user%2F00420557349755252971%2Fstate%2Fcom.google%2Fstarred?hl=en'
+  , first = 'https://www.google.com/reader/shared/user%2F00420557349755252971%2Fstate%2Fcom.google%2Fstarred?hl=en'
 
 fs.mkdirSync(dir, 0700)
 
-request(uri)
-  .pipe(images)
-  .pipe(boros(dir))
-    .on('data', function (chunk) {
-      console.log(chunk)
-    })
+function download (uri) {
+  request(uri)
+    .pipe(img)
+      .on('end', function (more) {
+        next = more
+      })
+    .pipe(boros(dir))
+      .on('data', function (chunk) {
+        console.log('file %s', chunk)
+      })
+      .on('end', function () {
+        if (next) {
+          pageCount++
+          if (pageCount < max) {
+            download(next)
+          }
+          next = null
+        } else {
+          console.log('OK')
+        }
+      })
+}
 
+download(first)
